39877a8684fd6702118f818cb37a8fc90a093f82
Ayoupey <ayoupemohamed@gmail.com>
2025-04-15 15:06:18 +0200

    Update page.tsx

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -30,7 +30,7 @@ export default function Contact() {
   return (
     <>
       <Header />
-      <main className="max-w-2xl mx-auto p-6 fade-in">
+      <main className="max-w-2xl mx-auto p-6 pt32 fade-in">
         <h2 className="text-3xl font-bold text-center mb-8">Contact Us</h2>
 
         <form className="space-y-6" onSubmit={handleSubmit}>
