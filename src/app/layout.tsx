a83c97ea004db4c995b25ac820aa22c575856118
Ayoupey <meshoayoupe@gmail.com>
2025-04-20 10:03:48 +0200

    update partners

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,21 @@
 import './globals.css';
+import { ReactNode } from 'react';
 
 export const metadata = {
   title: 'ALITTIHAD-AQUAPURE',
   description: 'Innovative Water Treatment Solutions',
 }
 
-import { ReactNode } from 'react';
-
 export default function RootLayout({ children }: { children: ReactNode }) {
-
   return (
     <html lang="en">
+      <head>
+        {/* Tidio Script in head (اختياري، ينفع في البودي برضو) */}
+        <script src="//code.tidio.co/hx2y5zvcejl8yv88bzkgh9bngmmvlxpe.js" async></script>
+      </head>
       <body className="font-times bg-white text-gray-900">
         {children}
       </body>
     </html>
-  )
+  );
 }
